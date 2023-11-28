e5da9931f63f74c0fb63f5b6154e55bb5b570476
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-11-28 09:44:20 +0700

    Menambah Halaman ContactUs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Home from './pages/Home';
 import CekKesehatanMental from './pages/CekKesehatan';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <div className="App bg-stone-200">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/pages/Home" element={<Home />} />
           <Route path="/pages/CekKesehatan" element={<CekKesehatanMental />} />
+          <Route path="/pages/ContactUs" element={<ContactUs />} />
         </Routes>
       </Router>
     </div>
